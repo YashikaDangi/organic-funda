5e2b712a34d6b937818dfaa88ef67cc554e9e241
YashikaDangi <yashikadangi10@gmail.com>
2025-05-17 19:32:34 +0530

    UI modifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
-import Header from "@/components/Header";
 import ReduxProvider from "@/redux/ReduxProvider";
 import CartSyncProvider from "@/components/CartSyncProvider";
 
@@ -43,7 +42,6 @@ export default function RootLayout({
         <ReduxProvider>
           <CartSyncProvider>
             <AuthProvider>
-              {/* <Header/> */}
               {children}
               <WhatsAppButton/>
             </AuthProvider>
